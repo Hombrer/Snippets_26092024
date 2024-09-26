3b789a4e5215f748676514fc2dc1c6f051c10c6f
Hombrer <5208643@mail.ru>
2024-09-26 14:33:58 +0300

    Start django form

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,7 +10,11 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {
+        'pagename': 'Добавление нового сниппета',
+        'form': form,
+        }
     return render(request, 'pages/add_snippet.html', context)
 
 
@@ -31,3 +36,13 @@ def snippet_detail(request, snippet_id):
     else:
         context["snippet"] = snippet
         return render(request, "pages/snippet_detail.html", context)
+    
+
+def create_snippet(request):
+    if request.method == "POST":
+        print(f'{request.POST = }')
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request,'add_snippet.html', {'form': form})

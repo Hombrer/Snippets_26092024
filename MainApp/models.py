685870b0539fc298f33204a67c1c7e4f880a8b3e
Hombrer <5208643@mail.ru>
2024-09-27 15:39:51 +0300

    Change Snippet model

--- MainApp/models.py
+++ MainApp/models.py
@@ -11,9 +11,15 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ['name', 'lang']
+
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     public = models.BooleanField(default=True) # True -> public, False -> private
+
+    def __repr__(self):
+        return f'Snippet({self.name}, {self.lang})'

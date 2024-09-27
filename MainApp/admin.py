194029c2e4cae1e4539a1206762f8ce24e9f92ef
Hombrer <5208643@mail.ru>
2024-09-27 16:10:19 +0300

    Add admin panel

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from MainApp.models import Snippet
+
 
 # Register your models here.
+# 1 вариант
+# admin.site.register(Snippet)
+
+# 2 вариант
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ["name", "lang", "creation_date", "code"]
+    list_filter = ["lang", "creation_date"]
+    ordering = ["creation_date"]
+    search_fields = ["name", "code"]
--- MainApp/models.py
+++ MainApp/models.py
@@ -14,12 +14,15 @@ class Snippet(models.Model):
     class Meta:
         ordering = ['name', 'lang']
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Имя сниппета")
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     public = models.BooleanField(default=True) # True -> public, False -> private
+
+    def __str__(self):
+        return f'{self.name}'
 
     def __repr__(self):
         return f'Snippet({self.name}, {self.lang})'
